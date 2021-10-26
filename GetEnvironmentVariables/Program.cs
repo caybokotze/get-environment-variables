f6f85be4217ac845fcca46ae6f2d729fc0e4e526
Caybo Kotzé <kotzecabo@gmail.com>
2021-10-26 14:37:53 +0200

    :tada: First commit

---
+++ GetEnvironmentVariables/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+
+namespace GetEnvironmentVariables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Listing of all your available environment variables");
+            foreach (DictionaryEntry environmentVariable in Environment.GetEnvironmentVariables())
+            {
+                Console.WriteLine($"KEY: {environmentVariable.Key} VALUE: {environmentVariable.Value}");
+            }
+        }
+    }
+}
